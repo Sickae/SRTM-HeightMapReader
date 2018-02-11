1b2a68843292828a275a2076ef07c5044d06bb5a
Sipos Bence <sipos.bnc@gmail.com>
2018-02-11 15:02:50 +0100

    + bitmaps

--- HeightMapReader/Program.cs
+++ HeightMapReader/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Drawing;
+using System.Text;
 
 namespace HeightMapReader
 {
@@ -71,7 +72,9 @@ namespace HeightMapReader
         {
             try
             {
-                BinaryReader br = new BinaryReader(File.Open(@file_path, FileMode.Open));
+                StringBuilder ext = new StringBuilder();
+                for (int i = 1; i <= 4; i++) ext.Insert(0, file_path[file_path.Length - i]);
+                BinaryReader br = new BinaryReader(File.Open(@file_path + (ext.ToString().Equals(".hgt") ? "" : ".hgt"), FileMode.Open));
                 List<short> bytes = new List<short>();
                 while (br.BaseStream.Position != br.BaseStream.Length) bytes.Add(br.ReadInt16());
                 br.Close();
@@ -96,17 +99,17 @@ namespace HeightMapReader
             bmp.Save(filename + ".bmp");
         }
 
-        public void Save(string dir_path, string filename)
-        {
-            Bitmap bmp = new Bitmap((int)Math.Sqrt(value.Length), (int)Math.Sqrt(value.Length));
-            for (int i = 0; i < value.Length; i++)
-            {
-                Color c;
-                if (value[i] > sealevel) c = Color.FromArgb(0, Math.Abs(value[i] % 255), 0);
-                else c = Color.FromArgb(0, 0, Math.Abs(value[i] % 255));
-                bmp.SetPixel(i / bmp.Width, i % bmp.Width, c);
-            }
-            bmp.Save(@dir_path + (dir_path[dir_path.Length-1] == '\\' ? "" : "\\") + filename + ".bmp");
-        }
+        //public void Save(string dir_path, string filename)
+        //{
+        //    Bitmap bmp = new Bitmap((int)Math.Sqrt(value.Length), (int)Math.Sqrt(value.Length));
+        //    for (int i = 0; i < value.Length; i++)
+        //    {
+        //        Color c;
+        //        if (value[i] > sealevel) c = Color.FromArgb(0, Math.Abs(value[i] % 255), 0);
+        //        else c = Color.FromArgb(0, 0, Math.Abs(value[i] % 255));
+        //        bmp.SetPixel(i / bmp.Width, i % bmp.Width, c);
+        //    }
+        //    bmp.Save(@dir_path + (dir_path[dir_path.Length-1] == '\\' ? "" : "\\") + filename + ".bmp");
+        //}
     }
 }
